14b66a559c1080b25f20697e84e4bc4496b01de1
Martialcart <jbe091@student.uib.no>
2020-12-22 13:09:50 +0100

    added project files

---
+++ MyClass.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FirstCSProject
+{
+    class MyClass
+    {
+        string name;
+        int age;
+        public MyClass(string navn, int alder)
+        {
+            name = navn;
+            age = alder;
+        }
+
+        public void print()
+        {
+            Console.WriteLine("navn:" + name + " alder:" + age);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace FirstCSProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("the diameter is" + diameter(5));
+
+            MyClass mann = new MyClass("Ringo", 42);
+            MyClass dame = new MyClass("Trude", 50);
+            mann.print();
+            dame.print();
+        }
+        static double diameter(double circle)
+        {
+            return circle / 3.14;
+        }
+
+    }
+}
