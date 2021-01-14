4a1e0717903a1f1c4df4a83827bf073427198849
Jan_Olav_Berg <janolav91@hotmail.com>
2021-01-14 13:55:16 +0100

    added for-loop and if

--- Program.cs
+++ Program.cs
@@ -7,8 +7,12 @@ namespace FirstCSProject
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            Console.WriteLine("the diameter is" + diameter(5));
-
+            Console.WriteLine("the diameter is " + diameter(5));
+            for (int i = 0; i < 10; i++) {
+                if (i % 2 == 0) { 
+                Console.WriteLine(i);
+                }
+            }
             MyClass mann = new MyClass("Ringo", 42);
             MyClass dame = new MyClass("Trude", 50);
             mann.print();
